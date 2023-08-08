ed5d36e275d4a65048f4400c97a7694f0093c7d1
Organique0 <grabnar.luka@gmail.com>
2023-08-09 01:28:55 +0200

    just install ALL packages

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -6,7 +6,7 @@ import Button from "./button";
 const NavbarActions = () => {
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex items-center rounded-full bg-black px-4 py-2 overflow-hidden">
+            <Button className="flex items-center px-4 py-2 overflow-hidden">
                 <FiShoppingBag size={20} color="white" />
             </Button>
         </div>

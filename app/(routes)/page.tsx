335b532da675deba8295fce2b8eff0326799980e
Organique0 <grabnar.luka@gmail.com>
2023-08-09 18:41:18 +0200

    billboard and featured products list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-    return (<div>hello store</div>);
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/productList";
+import Container from "@/components/ui/container";
+
+export const relavidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("8ee8861f-3a59-4110-ac24-6187315e7233");
+    const products = await getProducts({ isFeatured: true });
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured products" items={products} />
+                </div>
+            </div>
+        </Container>
+    );
 }
 
 export default HomePage;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import { FiShoppingBag } from "react-icons/fi";
-import Button from "./button";
+import Button from "./ui/button";
+import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) return null;
+
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex items-center px-4 py-2 overflow-hidden">
+            <Button className="flex items-center px-4 py-2">
                 <FiShoppingBag size={20} color="white" />
+                <span className="ml-2 text-sm font-medium text-white">
+                    0
+                </span>
             </Button>
         </div>
     );
